11d2c68188d984550cc8731405aaae2a216bd158
Eekshit Chawla <eekshitchawla@eeki.local>
2023-11-19 10:02:37 +0530

    edit functionality

--- src/components/admin/EditMember/EditMember.js
+++ src/components/admin/EditMember/EditMember.js
@@ -1,51 +1,41 @@
 import { initializeApp } from "firebase/app";
-import { doc, getDoc, getFirestore } from "firebase/firestore";
-import React, { useRef, useState } from "react";
+import { doc, getDoc, getFirestore, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
 
 const EditMember = () => {
-  const id = useRef();
-  const memId = useRef();
-  const memName = useRef();
-  const age = useRef();
-  const phnm = useRef();
-  const address = useRef();
+  const [id, setId] = useState("");
+  const [memId, setMemId] = useState("");
+  const [memName, setMemName] = useState("");
+  const [age, setAge] = useState("");
+  const [phnm, setPhnm] = useState("");
+  const [address, setAddress] = useState("");
   const [isPresent, setIsPresent] = useState(false);
+  const firebaseConfig = {
+    apiKey: "AIzaSyDvXEVHj8jIG19gVRZ5GBWChBBhZk9uwUA",
+    authDomain: "westend-tc.firebaseapp.com",
+    projectId: "westend-tc",
+    storageBucket: "westend-tc.appspot.com",
+    messagingSenderId: "539923039306",
+    appId: "1:539923039306:web:9b5954217ce1c55ca552b3",
+    measurementId: "G-K65TR7EZVF",
+  };
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
   const editMember = async () => {
-    const firebaseConfig = {
-      apiKey: "AIzaSyDvXEVHj8jIG19gVRZ5GBWChBBhZk9uwUA",
-      authDomain: "westend-tc.firebaseapp.com",
-      projectId: "westend-tc",
-      storageBucket: "westend-tc.appspot.com",
-      messagingSenderId: "539923039306",
-      appId: "1:539923039306:web:9b5954217ce1c55ca552b3",
-      measurementId: "G-K65TR7EZVF",
-    };
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-    const memberId = id.current.value;
+    const memberId = id;
     try {
       const memberDocRef = doc(db, "members", memberId);
       const memExist = await getDoc(memberDocRef);
       if (memExist.exists()) {
         setIsPresent(true);
         const memSnap = await getDoc(memberDocRef);
-        // console.log(memSnap.data());
         const props = memSnap.data();
-        console.log(props);
 
-        memId.current = props.memberId;
-        memName.current = props.memberName;
-        age.current = props.memberAge;
-        phnm.current = props.phoneNumber;
-        address.current = props.memberAddress;
-        console.log(
-          "djbnfj:  ",
-          memId.current,
-          memName.current,
-          age.current,
-          phnm.current,
-          address.current
-        );
+        setMemId(props.memberId);
+        setMemName(props.memberName);
+        setAge(props.memberAge);
+        setPhnm(props.phoneNumber);
+        setAddress(props.memberAddress);
       } else {
         alert("Member does not exist.");
       }
@@ -54,12 +44,39 @@ const EditMember = () => {
     }
   };
 
+  const updateFirestore = async () => {
+    try {
+      const memberDocRef = doc(db, "members", id);
+      const memExist = await getDoc(memberDocRef);
+
+      if (memExist.exists()) {
+        setIsPresent(true);
+
+        await updateDoc(memberDocRef, {
+          memberName: memName,
+          memberAge: age,
+          phoneNumber: phnm,
+          memberAddress: address,
+        });
+        alert("Member Details updated successfully!");
+      } else {
+        alert("Member does not exist.");
+      }
+    } catch (error) {
+      console.error("Error updating member data:", error.message);
+    }
+  };
+
   return (
     <div className="removeMember">
       Edit Member
       <form>
         <label>Member ID</label>
-        <input type="number" ref={id} />
+        <input
+          type="number"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
         <button
           onClick={(e) => {
             e.preventDefault();
@@ -73,48 +90,52 @@ const EditMember = () => {
         <form>
           <label>Member Id</label>
           <input
-            ref={memId}
+            disabled={true}
             type="text"
             required
-            value={memId.current}
-            onChange={() => {}}
+            value={memId || ""}
+            onChange={(e) => setMemId(e.target.value)}
           />
 
           <label>Name</label>
           <input
-            ref={memName}
             type="text"
             required
-            value={memName.current}
-            onChange={() => {}}
+            value={memName || ""}
+            onChange={(e) => setMemName(e.target.value)}
           />
 
           <label>Age</label>
           <input
-            onChange={() => {}}
-            ref={age}
             type="text"
             required
-            value={age.current}
+            value={age || ""}
+            onChange={(e) => setAge(e.target.value)}
           />
 
           <label>Phone Number</label>
           <input
-            onChange={() => {}}
-            ref={phnm}
             type="text"
             required
-            value={phnm.current}
+            value={phnm || ""}
+            onChange={(e) => setPhnm(e.target.value)}
           />
 
           <label>Address</label>
           <input
-            ref={address}
             type="text"
             required
-            value={address.current}
-            onChange={() => {}}
+            value={address || ""}
+            onChange={(e) => setAddress(e.target.value)}
           />
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              updateFirestore();
+            }}
+          >
+            Update
+          </button>
         </form>
       )}
     </div>
